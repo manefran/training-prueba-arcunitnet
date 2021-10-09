d165b90ad160d444a52c4b131143da2220a5e575
PSOFKA0417\manuel.arrieta <manuel.vallejo@sofka.com.co>
2021-10-09 11:12:48 -0500

    add project example archunitnet

---
+++ training-pruebas-archunitnet/ExampleArchUnitTest.cs
@@ -0,0 +1,90 @@
+﻿using ArchUnitNET.Domain;
+using ArchUnitNET.Fluent;
+using ArchUnitNET.Loader;
+using ArchUnitNET.xUnit;
+using Xunit;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace training_pruebas_archunitnet
+{
+    public class ExampleArchUnitTest
+    {
+        // TIP: load your architecture once at the start to maximize performance of your tests
+        private static readonly Architecture Architecture =
+            new ArchLoader().LoadAssemblies(typeof(ExampleClass).Assembly, typeof(ForbiddenClass).Assembly).Build();
+        // replace <ExampleClass> and <ForbiddenClass> with classes from the assemblies you want to test
+
+        //declare variables you'll use throughout your tests up here
+        //use As() to give them a custom description
+        private readonly IObjectProvider<IType> ExampleLayer =
+            Types().That().ResideInAssembly("ExampleAssembly").As("Example Layer");
+
+        private readonly IObjectProvider<Class> ExampleClasses =
+            Classes().That().ImplementInterface("IExampleInterface").As("Example Classes");
+
+        private readonly IObjectProvider<IType> ForbiddenLayer =
+            Types().That().ResideInNamespace("ForbiddenNamespace").As("Forbidden Layer");
+
+        private readonly IObjectProvider<Interface> ForbiddenInterfaces =
+            Interfaces().That().HaveFullNameContaining("forbidden").As("Forbidden Interfaces");
+
+
+        //write some tests
+        [Fact]
+        public void TypesShouldBeInCorrectLayer()
+        {
+            //you can use the fluent API to write your own rules
+            IArchRule exampleClassesShouldBeInExampleLayer =
+                Classes().That().Are(ExampleClasses).Should().Be(ExampleLayer);
+            IArchRule forbiddenInterfacesShouldBeInForbiddenLayer =
+                Interfaces().That().Are(ForbiddenInterfaces).Should().Be(ForbiddenLayer);
+
+            //check if your architecture fulfils your rules
+            exampleClassesShouldBeInExampleLayer.Check(Architecture);
+            forbiddenInterfacesShouldBeInForbiddenLayer.Check(Architecture);
+
+            //you can also combine your rules
+            IArchRule combinedArchRule =
+                exampleClassesShouldBeInExampleLayer.And(forbiddenInterfacesShouldBeInForbiddenLayer);
+            combinedArchRule.Check(Architecture);
+        }
+
+        [Fact]
+        public void ExampleLayerShouldNotAccessForbiddenLayer()
+        {
+            //you can give your rules a custom reason, which is displayed when it fails (together with the types that failed the rule)
+            IArchRule exampleLayerShouldNotAccessForbiddenLayer = Types().That().Are(ExampleLayer).Should()
+                .NotDependOnAny(ForbiddenLayer).Because("it's forbidden");
+            exampleLayerShouldNotAccessForbiddenLayer.Check(Architecture);
+        }
+
+        [Fact]
+        public void ForbiddenClassesShouldHaveCorrectName()
+        {
+            Classes().That().AreAssignableTo(ForbiddenInterfaces).Should().HaveNameContaining("forbidden")
+                .Check(Architecture);
+        }
+
+        [Fact]
+        public void ExampleClassesShouldNotCallForbiddenMethods()
+        {
+            Classes().That().Are(ExampleClasses).Should().NotCallAny(
+                    MethodMembers().That().AreDeclaredIn(ForbiddenLayer).Or().HaveNameContaining("forbidden"))
+                .Check(Architecture);
+        }
+    }
+}
+
+internal class ExampleClass
+{
+    private int id { get; set; }
+    private string name { get; set; }
+
+}
+
+internal class ForbiddenClass
+{
+    private int sum(int num1, int num2) => num1 + num2;
+    private int subs(int num1, int num2) => num1 - num2;
+
+}
---
+++ training-pruebas-archunitnet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace training_pruebas_archunitnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
